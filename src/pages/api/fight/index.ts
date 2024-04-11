9e22d381d4fe48a45b308c02a673c49ee23a2903
Richard Westberg <richard.westberg@welcom.se>
2024-04-11 23:06:22 +0200

    add validation to fight api

--- src/pages/api/fight/index.ts
+++ src/pages/api/fight/index.ts
@@ -1,38 +1,49 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ApiError } from "../monsters/types";
 import { attack, evaluateWinner } from "./lib";
-import { Fight, FightResult } from "./types";
+import { Fight, FightResult, fightSchema } from "./types";
+import { z } from "zod";
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<FightResult | ApiError>
 ) {
-  const { method, body } = req;
-
-  if (method !== "POST") {
+  if (req.method !== "POST") {
     res.setHeader("Allow", ["POST"]);
     res
       .status(405)
-      .json({ error: { message: `Method ${method} Not Allowed` } });
+      .json({ error: { message: `Method ${req.method} Not Allowed` } });
     return;
   }
 
-  let fight: Fight;
+  let body = {};
   try {
-    fight = JSON.parse(body) as Fight;
+    body = JSON.parse(req.body);
   } catch (err) {
-    res.status(400).json({ error: { message: `Failed to parse body` } });
+    res.status(400).json({ error: { message: 'Failed to parse body' } });
     return;
   }
+
+  const schema = fightSchema;
+  const response = schema.safeParse(body);
+
+  if (!response.success) {
+    const { errors } = response.error;
+    console.log(response.error);
+    res.status(400).json({ error: { message: "Invalid request", errors } });
+    return;
+  }
+
+  const { dragon1, dragon2 } = response.data;
 
   const result: FightResult = {
-    dragon1: { ...fight.dragon1, health: attack(fight.dragon2, fight.dragon1) },
-    dragon2: { ...fight.dragon2, health: attack(fight.dragon1, fight.dragon2) },
+    dragon1: { ...dragon1, health: attack(dragon2, dragon1) },
+    dragon2: { ...dragon2, health: attack(dragon1, dragon2) },
     winner: null,
   };
 
   const resultWithMaybeWinner = evaluateWinner(result);
-  fightLog.push({ fight, result: resultWithMaybeWinner });
+  fightLog.push({ fight: { dragon1, dragon2 }, result: resultWithMaybeWinner });
 
   res.status(201).json(resultWithMaybeWinner);
 }
