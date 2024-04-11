34d9bd5eab40584d562c68c68303a53644625fb8
Richard Westberg <richard.westberg@welcom.se>
2024-04-11 22:21:29 +0200

    don't go below 0 in health

--- src/pages/api/fight.ts
+++ src/pages/api/fight.ts
@@ -15,14 +15,15 @@ export type FightResult = Fight & {
 const attack = (
   attacker: MonsterWithHealth,
   victim: MonsterWithHealth
-): number => Math.round(victim.health - attacker.strength * Math.random());
+): number =>
+  Math.max(Math.round(victim.health - attacker.strength * Math.random()), 0);
 
 const evaluateWinner = (fightResult: FightResult): FightResult => {
-  if (fightResult.dragon1.health < 0 && fightResult.dragon2.health < 0) {
+  if (fightResult.dragon1.health === 0 && fightResult.dragon2.health === 0) {
     fightResult.winner = "none";
-  } else if (fightResult.dragon1.health < 0) {
+  } else if (fightResult.dragon1.health === 0) {
     fightResult.winner = "dragon2";
-  } else if (fightResult.dragon2.health < 0) {
+  } else if (fightResult.dragon2.health === 0) {
     fightResult.winner = "dragon1";
   }
 
